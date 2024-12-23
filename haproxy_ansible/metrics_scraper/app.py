7a3914f799b54fdc244fa4ab1fda26d0c9c9b8d2
sabhishekpratap5 <sabhishekpratap5@gmail.com>
2024-12-24 00:05:52 +0530

    Add files via upload
    Docker Container Challenge for Assessment

---
+++ haproxy_ansible/metrics_scraper/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+import psutil
+
+app = Flask(__name__)
+
+@app.route('/metrics', methods=['GET'])
+def metrics():
+    return jsonify({
+        'cpu_percent': psutil.cpu_percent(),
+        'memory': psutil.virtual_memory()._asdict()
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
